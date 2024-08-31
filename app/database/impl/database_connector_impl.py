81fd15985f8e700fcfea0c33be479899282f6018
galvagnimatteo <galvagni.matteo@protonmail.com>
2024-08-31 17:44:41 +0200

    feat: add file config

--- app/database/impl/database_connector_impl.py
+++ app/database/impl/database_connector_impl.py
@@ -1,3 +1,5 @@
+import json
+
 from sqlalchemy.orm import sessionmaker, Session
 from sqlalchemy import create_engine
 import os
@@ -5,8 +7,21 @@ import os
 from app.database.database_connector import DatabaseConnector
 from app.database.impl.base_provider import Base
 
-DATABASE_URL = os.getenv("DATABASE_URL")
-engine = create_engine(DATABASE_URL)
+
+# Reads credentials from PG_CREDENTIALS_FILE env, defined inside docker-compose.
+def read_pg_credentials(path):
+    if not path or not os.path.exists(path):
+        raise FileNotFoundError(f"Postgres credentials file doesn't exist: {path}")
+
+    with open(credentials_file, 'r') as file:
+        credentials_map = json.load(file)
+
+    return credentials_map
+
+credentials_file = os.getenv('PG_CREDENTIALS_FILE')
+credentials = read_pg_credentials(credentials_file)
+
+engine = create_engine(f"postgresql://{credentials['POSTGRES_USER']}:{credentials['POSTGRES_PASSWORD']}@db:5432/{credentials['POSTGRES_DB']}")
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 class DatabaseConnectorImpl(DatabaseConnector):

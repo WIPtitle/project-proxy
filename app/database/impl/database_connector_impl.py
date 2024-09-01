62fdb057b0fcaa5ad47360f1b8fd1f8a7785227f
galvagnimatteo <galvagni.matteo@protonmail.com>
2024-09-01 19:38:23 +0200

    feat: add rabbitmq sdk

--- app/database/impl/database_connector_impl.py
+++ app/database/impl/database_connector_impl.py
@@ -1,25 +1,14 @@
-import json
 import os
 
 from sqlmodel import Session, create_engine, SQLModel
-
 from app.database.database_connector import DatabaseConnector
+from app.utils.read_credentials import read_credentials
 
-
-# Reads credentials from PG_CREDENTIALS_FILE env, defined inside docker-compose.
-def read_pg_credentials(path):
-    if not path or not os.path.exists(path):
-        raise FileNotFoundError(f"Postgres credentials file doesn't exist: {path}")
-
-    with open(path, 'r') as file:
-        credentials_map = json.load(file)
-
-    return credentials_map
 
 class DatabaseConnectorImpl(DatabaseConnector):
     def __init__(self):
         credentials_file = os.getenv('PG_CREDENTIALS_FILE')
-        credentials = read_pg_credentials(credentials_file)
+        credentials = read_credentials(credentials_file)
 
         self.session_instance = None
         self.engine = create_engine(
---
+++ app/utils/read_credentials.py
@@ -0,0 +1,14 @@
+import json
+import os
+
+
+# Reads credentials from credentials file and returns them in a map.
+# Credentials files path are defined in common environment variables in docker compose of main project.
+def read_credentials(path):
+    if not path or not os.path.exists(path):
+        raise FileNotFoundError(f"Postgres credentials file doesn't exist: {path}")
+
+    with open(path, 'r') as file:
+        credentials_map = json.load(file)
+
+    return credentials_map

2a4d12ca79eb77e07577651a2aa12877ff37d48c
galvagnimatteo <galvagni.matteo@protonmail.com>
2024-09-02 21:31:09 +0200

    fix: fixed wrong postgres credentials file and changed smth

--- app/utils/read_credentials.py
+++ app/utils/read_credentials.py
@@ -6,7 +6,7 @@ import os
 # Credentials files path are defined in common environment variables in docker compose of main project.
 def read_credentials(path):
     if not path or not os.path.exists(path):
-        raise FileNotFoundError(f"Postgres credentials file doesn't exist: {path}")
+        raise FileNotFoundError(f"Credentials file doesn't exist: {path}")
 
     with open(path, 'r') as file:
         credentials_map = json.load(file)

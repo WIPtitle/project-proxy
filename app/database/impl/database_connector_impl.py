8ee276c5a163173e7c141692391bd16cdcc2f88a
galvagnimatteo <galvagni.matteo@protonmail.com>
2024-09-03 20:58:35 +0200

    feat: add status endpoint and used enum for status

--- app/database/impl/database_connector_impl.py
+++ app/database/impl/database_connector_impl.py
@@ -11,14 +11,17 @@ class DatabaseConnectorImpl(DatabaseConnector):
         credentials_file = os.getenv('PG_CREDENTIALS_FILE')
         credentials = read_credentials(credentials_file)
 
-        self.session_instance = None
+        self.session = None
         self.engine = create_engine(
             f"postgresql://{credentials['POSTGRES_USER']}:{credentials['POSTGRES_PASSWORD']}@{database_hostname}:5432/{credentials['POSTGRES_DB']}",
             echo=True)
 
         SQLModel.metadata.create_all(self.engine)
 
-    def get_session(self) -> Session:
-        if self.session_instance is None:
-            self.session_instance = Session(self.engine)
-        return self.session_instance
+    def get_session(self):
+        if self.session is None or not self.session.is_active:
+            if self.session is not None:
+                self.session.rollback()
+                self.session.close()
+            self.session = Session(self.engine)
+        return self.session

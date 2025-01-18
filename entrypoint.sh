623aea8c2fb3b3ee5602481895d459a9d2a227c7
galvagnimatteo <galvagni.matteo@protonmail.com>
2025-01-18 18:28:48 +0100

    feat(wip): camera always records (WIP)

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,4 +7,16 @@ pip install --upgrade pip
 apt-get update && apt-get install -y libpq-dev git
 pip install --no-cache-dir -r requirements.txt
 
-uvicorn app.main:app --host 0.0.0.0 --port 8000
+uvicorn app.main:app --host 0.0.0.0 --port 8000 &
+
+# Loop until server up to force initialization
+while true; do
+    response=$(curl --write-out "%{http_code}" --silent --output /dev/null http://0.0.0.0:8000)
+    if [ "$response" -eq 404 ]; then
+        break
+    else
+        sleep 1
+    fi
+done
+
+wait

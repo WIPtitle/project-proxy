c855a525d24b45e51b392ab255d520a6199d4c22
galvagnimatteo <galvagni.matteo@protonmail.com>
2025-01-26 17:08:02 +0100

    fix: removed livestream since its a pain in the ass

--- app/routers/impl/proxy_router.py
+++ app/routers/impl/proxy_router.py
@@ -37,7 +37,7 @@ class ProxyRouter(RouterWrapper):
 
         try:
             # 'bit of an ugly hack, but I don't think there will be any other stream methods so cope with it, I'm tired
-            if "stream" in url and request.method == "GET" and output_service == os.getenv('DEVICES_MANAGER_HOSTNAME'):
+            if ("stream" in url or "static" in url) and request.method == "GET" and output_service == os.getenv('DEVICES_MANAGER_HOSTNAME'):
                 headers = {key: value for key, value in request.headers.items() if key.lower() != 'host'}
                 async with httpx.AsyncClient(timeout=httpx.Timeout(60.0)) as client:
                     response = await client.get(url, headers=headers)

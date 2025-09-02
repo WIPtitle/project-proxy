14c7574ada45cd401fb9974a9691465cd4807298
galvagnimatteo <galvagni.matteo@protonmail.com>
2025-09-02 17:28:14 +0200

    feat: working real time streaming

--- app/routers/impl/proxy_router.py
+++ app/routers/impl/proxy_router.py
@@ -22,7 +22,6 @@ class ProxyRouter(RouterWrapper):
             "audio-service": os.getenv('LOCAL_AUDIO_MANAGER_HOSTNAME')
         }
 
-
     async def _proxy(self, request: Request, input_service: str, path: str):
         output_service = self.service_mapping.get(input_service)
         if output_service is None:
@@ -36,9 +35,9 @@ class ProxyRouter(RouterWrapper):
         print(f"Routing request to {url}")
 
         try:
-            # 'bit of an ugly hack, but I don't think there will be any other stream methods so cope with it, I'm tired
             if "stream" in url and request.method == "GET" and output_service == os.getenv('DEVICES_MANAGER_HOSTNAME'):
                 headers = {key: value for key, value in request.headers.items() if key.lower() != 'host'}
+
                 if "status" in url:
                     async def stream():
                         async with httpx.AsyncClient(timeout=httpx.Timeout(60.0)) as client:
@@ -48,17 +47,21 @@ class ProxyRouter(RouterWrapper):
 
                     return StreamingResponse(stream(), media_type="text/event-stream")
 
-                elif "recording" in url:
-                    async with httpx.AsyncClient(timeout=httpx.Timeout(60.0)) as client:
-                        response = await client.get(url, headers=headers)
+                elif "recording" in url or "camera" in url:
+                    async def stream_content():
+                        async with httpx.AsyncClient(timeout=httpx.Timeout(300.0)) as client:
+                            async with client.stream("GET", url, headers=headers) as response:
+                                async for chunk in response.aiter_bytes(chunk_size=65536):
+                                    yield chunk
 
-                        return StreamingResponse(
-                            content=response.aiter_bytes(),
-                            status_code=response.status_code,
-                            headers=dict(response.headers),
-                            media_type=response.headers.get('content-type')
-                        )
-
+                    return StreamingResponse(
+                        stream_content(),
+                        media_type="video/x-matroska",
+                        headers={
+                            "Cache-Control": "no-cache",
+                            "Connection": "keep-alive",
+                        }
+                    )
             else:
                 async with httpx.AsyncClient(timeout=httpx.Timeout(60.0)) as client:
                     response = await client.request(
